d744d351e9feec362d62c563c6ee3fc2fe464d5f
m8andour <mghandour21185701204@gmail.com>
2024-08-24 21:22:57 +0300

    'start'

--- lib/day_5/day_5.dart
+++ lib/day_5/day_5.dart
@@ -5,45 +5,47 @@ class Day5 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( 
         appBar: AppBar(
           leading: IconButton(
             icon: Icon(Icons.menu),
-            onPressed: () {
-              // handle menu button press
+            onPressed: (){
+              // handle menu button press 
             },
           ),
           title: Text(
             'Centered Title',
             style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0,
-                color: Colors.white12),
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+              color: Colors.white12
+            ),
           ),
           centerTitle: true,
           elevation: 10.0,
           backgroundColor: Colors.blueGrey,
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.network(
-                  "https://th.bing.com/th/id/OIP.qXtyevXs1Smk3OSg9InaswHaFj?w=288&h=216&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-                  height: 200.0,
-                  width: 200.0,
+        body: Padding( 
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "G:\Moaz\Pro&Tasks\back1.png"
+                height: 200.0,
+                width: 200.0,
+              ),
+              SizedBox(height: 20.0,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Text',
+                  hintText: 'Hint Text Here'
                 ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter Text',
-                      hintText: 'Hint Text Here'),
-                )
-              ],
-            )));
+              )
+            ],
+          )
+        )
+    );
   }
 }
